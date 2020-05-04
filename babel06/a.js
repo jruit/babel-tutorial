72519e35b5a7e529c9283aedd026faece429d022
jruit <929322398@qq.com>
2020-05-04 16:48:31 +0800

    babel06

---
+++ babel06/a.js
@@ -0,0 +1,5 @@
+// import "core-js/stable";
+// import "regenerator-runtime/runtime";
+
+var promise = Promise.resolve('ok');
+console.log(promise);
---
+++ babel06/webpack.config.js
@@ -0,0 +1,11 @@
+
+const path = require('path');
+
+module.exports = {
+  entry: ['./polyfill.js', './a.js'],
+  output: {
+    filename: 'b.js',
+    path: path.resolve(__dirname, '')
+  },
+  mode: 'development'
+};
