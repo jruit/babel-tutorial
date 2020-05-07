d0854f202c42bab9835505a6059d11a1011c31db
jruit <929322398@qq.com>
2020-05-07 21:15:42 +0800

    babel13a

---
+++ babel13a/a.js
@@ -0,0 +1,9 @@
+// 这个例子不是用来运行的。b.js是手动引入包后的代码，不是命令生成的。
+class Person {
+  sayname() {
+    return 'name'
+  }
+}
+
+var john = new Person()
+console.log(john)
---
+++ babel13a/babel.config.js
@@ -0,0 +1,5 @@
+// 这个例子不是用来运行的。b.js是手动引入包后的代码，不是命令生成的。
+module.exports = {
+  presets: ["@babel/env"],
+  plugins: []
+}
