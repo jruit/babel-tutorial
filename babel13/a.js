c93d32812693dee277de0ce333c0d3f59fcaddb1
jruit <929322398@qq.com>
2020-05-07 19:21:30 +0800

    babel13

---
+++ babel13/a.js
@@ -0,0 +1,8 @@
+class Person {
+  sayname() {
+    return 'name'
+  }
+}
+
+var john = new Person()
+console.log(john)
---
+++ babel13/b.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Person = /*#__PURE__*/function () {
+  function Person() {
+    _classCallCheck(this, Person);
+  }
+
+  _createClass(Person, [{
+    key: "sayname",
+    value: function sayname() {
+      return 'name';
+    }
+  }]);
+
+  return Person;
+}();
+
+var john = new Person();
+console.log(john);
